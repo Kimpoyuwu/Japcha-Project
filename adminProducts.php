0fba4415129137cc0c8ed71210963c2fdf5f9f2c
Ahndreiy Buscado <115107347+ahndreiy02@users.noreply.github.com>
2023-07-30 23:51:50 +0800

    Products

--- adminProducts.php
+++ adminProducts.php
@@ -1,8 +1,174 @@
 <?php
     include "adminHeader.php";
 ?>
+<link rel="stylesheet" href="assets/css/adminProducts.css">
+<div class="main">
+    <div class="headerSection">
+        <p>Product List</p>
+        <a href="#">Add new</a>
 
-echo "Hello World";
+    </div>
+
+    <div class="searchSection">
+        <input type="text" placeholder = "Search">
+
+        <select name="Category" class = "Category" id="Category">
+            <option value="selectCategory">Category</option>
+        </select>
+    </div>
+
+    <div class="productSection">
+        <div class="boxContainer">
+            <img src="image/sampleimage.png" alt="">
+            <div class="productDescription">
+                <span>Product Name</span>
+                <p>P100</p>
+            </div>
+                <div class="productAction">
+                    <div class="editContainer">
+                        <img src="image/editIcon.png" alt="">
+                        <a href="" class ="Edit">Edit</a>
+
+                    </div>
+                    <div class="removeContainer">
+                        <img src="image/removeIcon.png" alt="">
+                        <a href="" class ="Remove">Remove</a>
+
+                    </div>
+                        
+                </div>
+        </div>
+
+        <div class="boxContainer">
+            <img src="image/sampleimage.png" alt="">
+            <div class="productDescription">
+                <span>Product Name</span>
+                <p>P100</p>
+            </div>
+                <div class="productAction">
+                    <div class="editContainer">
+                        <img src="image/editIcon.png" alt="">
+                        <a href="" class ="Edit">Edit</a>
+
+                    </div>
+                    <div class="removeContainer">
+                        <img src="image/removeIcon.png" alt="">
+                        <a href="" class ="Remove">Remove</a>
+
+                    </div>
+                        
+                </div>
+        </div>
+
+        <div class="boxContainer">
+            <img src="image/sampleimage.png" alt="">
+            <div class="productDescription">
+                <span>Product Name</span>
+                <p>P100</p>
+            </div>
+                <div class="productAction">
+                    <div class="editContainer">
+                        <img src="image/editIcon.png" alt="">
+                        <a href="" class ="Edit">Edit</a>
+
+                    </div>
+                    <div class="removeContainer">
+                        <img src="image/removeIcon.png" alt="">
+                        <a href="" class ="Remove">Remove</a>
+
+                    </div>
+                        
+                </div>
+        </div>
+
+        <div class="boxContainer">
+            <img src="image/sampleimage.png" alt="">
+            <div class="productDescription">
+                <span>Product Name</span>
+                <p>P100</p>
+            </div>
+                <div class="productAction">
+                    <div class="editContainer">
+                        <img src="image/editIcon.png" alt="">
+                        <a href="" class ="Edit">Edit</a>
+
+                    </div>
+                    <div class="removeContainer">
+                        <img src="image/removeIcon.png" alt="">
+                        <a href="" class ="Remove">Remove</a>
+
+                    </div>
+                        
+                </div>
+        </div>
+
+        <div class="boxContainer">
+            <img src="image/sampleimage.png" alt="">
+            <div class="productDescription">
+                <span>Product Name</span>
+                <p>P100</p>
+            </div>
+                <div class="productAction">
+                    <div class="editContainer">
+                        <img src="image/editIcon.png" alt="">
+                        <a href="" class ="Edit">Edit</a>
+
+                    </div>
+                    <div class="removeContainer">
+                        <img src="image/removeIcon.png" alt="">
+                        <a href="" class ="Remove">Remove</a>
+
+                    </div>
+                        
+                </div>
+        </div>
+
+        <div class="boxContainer">
+            <img src="image/sampleimage.png" alt="">
+            <div class="productDescription">
+                <span>Product Name</span>
+                <p>P100</p>
+            </div>
+                <div class="productAction">
+                    <div class="editContainer">
+                        <img src="image/editIcon.png" alt="">
+                        <a href="" class ="Edit">Edit</a>
+
+                    </div>
+                    <div class="removeContainer">
+                        <img src="image/removeIcon.png" alt="">
+                        <a href="" class ="Remove">Remove</a>
+
+                    </div>
+                        
+                </div>
+        </div>
+
+        <div class="boxContainer">
+            <img src="image/sampleimage.png" alt="">
+            <div class="productDescription">
+                <span>Product Name</span>
+                <p>P100</p>
+            </div>
+                <div class="productAction">
+                    <div class="editContainer">
+                        <img src="image/editIcon.png" alt="">
+                        <a href="" class ="Edit">Edit</a>
+
+                    </div>
+                    <div class="removeContainer">
+                        <img src="image/removeIcon.png" alt="">
+                        <a href="" class ="Remove">Remove</a>
+
+                    </div>
+                        
+                </div>
+        </div>
+
+    </div>
+</div>
+
+
 <?php
     include "adminFooter.php";
 
