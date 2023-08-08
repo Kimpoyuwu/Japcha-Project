9c6c49ae75661ede66395848f3851388175ca52d
Ahndreiy Buscado <115107347+ahndreiy02@users.noreply.github.com>
2023-08-08 10:37:45 +0800

    adminProducts

--- adminProducts.php
+++ adminProducts.php
@@ -1,11 +1,11 @@
 <?php
     include "adminHeader.php";
 ?>
-    <!-- <link rel="stylesheet" href="assets/css/adminProducts.css"> -->
+    <link rel="stylesheet" href="assets/css/admin.css">
 <div class="adminSection">
     <div class="headerSection">
         <p>Product List</p>
-            <a href="#">Add new</a>
+            <a href="#" id="addNew">Add new</a>
 
     </div>
 
@@ -110,6 +110,69 @@
 
     </div>
 </div>
+
+    <div class="modal"> 
+        <form class = "formProducts"action="">
+            <div class="titleModal">
+            <p>Add Product</p>
+            <img class ="closeButton" src="image/closeButton.png" alt="">
+            </div>
+         
+            <div class="secondLayer">
+                <div class="leftCont">    
+                    <img src="image/uploadImage.png" alt="">
+                    <input class = "file" type="file" accept ="image/*">    
+                </div>
+
+                <div class="rightCont">
+                    <label for="productName">Product Name</label>
+                    <input type="text"id ="productName">
+                    <label for="label">Label</label>
+                    <input type="text" id="label">
+                    <label for="addOns">Add-ons:</label>
+                    <div class="optionCont">
+                        <div class="leftOption">
+                            <div class="firstOption">
+                                <input type="checkbox" id="pearl">
+                                <p>Pearl</p>
+                            </div>
+                            <div class="secondOption">
+                                <input type="checkbox" id="nata">
+                                <p>Nata</p>
+                            </div>
+                            
+                        </div>
+                        <div class="rightOption">
+                             <div class="thirdOption">
+                                <input type="checkbox" id="rainbowJelly">
+                                <p>Rainbow Jelly</p>
+                            </div>
+                            <div class="fourthOption">
+                                <input type="checkbox" id="coffeeJelly">
+                                <p>Coffee Jelly</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <script>
+        const addNewLink = document.getElementById('addNew');
+        const modal = document.querySelector('.modal');
+        const closeButton = document.querySelector('.closeButton');
+
+        addNewLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        modal.style.display = 'block';
+    });
+
+        closeButton.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });             
+    </script> 
 
 <?php
     include "adminFooter.php";
