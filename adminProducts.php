80a368a8cea2ecedcfbff06274359e32da8a7cc5
Ahndreiy Buscado <115107347+ahndreiy02@users.noreply.github.com>
2023-08-08 10:47:19 +0800

    adminProducts

--- adminProducts.php
+++ adminProducts.php
@@ -101,6 +101,26 @@
                         
                 </div>
         </div>
+        <div class="boxContainer">
+            <img src="image/sampleimage.png" alt="">
+            <div class="productDescription">
+                <span>Product Name</span>
+                <p>P100</p>
+            </div>
+                <div class="productAction">
+                    <div class="editContainer">
+                        <img src="image/editIcon.png" alt="">
+                        <a href="" class ="Edit">Edit</a>
+
+                    </div>
+                    <div class="removeContainer">
+                        <img src="image/removeIcon.png" alt="">
+                        <a href="" class ="Remove">Remove</a>
+
+                    </div>
+                        
+                </div>
+        </div>
 
         
 
