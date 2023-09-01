6e8f191398dd27275179fca897e5cf71c31e8c14
Adner <adnerdevila23@gmail.com>
2023-09-01 18:43:45 +0800

    css customer

--- c_header.php
+++ c_header.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
     <link rel="stylesheet" href="customer.css">
-    <link rel="stylesheet" href="customerPath.css">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"> -->
     <title>Document</title>
     <style>
