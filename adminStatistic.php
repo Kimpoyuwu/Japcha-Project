c5680ecb7ce09dbb005c535ba0c1dd69c20aab79
Kim David <115102999+Kimpoyuwu@users.noreply.github.com>
2023-09-09 22:12:35 +0800

    stats

---
+++ adminStatistic.php
@@ -0,0 +1,28 @@
+<?php
+    include "adminHeader.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <div class="main--cont">
+
+        <div class="drop-cont">
+
+        <select  class = "Daily" id="daily" name="Daily" required>
+                    <option >Daily</option>
+                    <option >Weekly</option>
+                    <option >Monthly</option>
+                    <option >Yearly</option>
+        </select>
+
+        </div>
+    </div>
+</body>
+</html>
