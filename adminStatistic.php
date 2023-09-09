e774f2de4a652e1733396e5f6de670d4beb0314b
Kim David <115102999+Kimpoyuwu@users.noreply.github.com>
2023-09-10 00:19:25 +0800

    statistics

--- adminStatistic.php
+++ adminStatistic.php
@@ -12,17 +12,67 @@
 </head>
 <body>
     <div class="main--cont">
-
+        
         <div class="drop-cont">
 
-        <select  class = "Daily" id="daily" name="Daily" required>
+            <select  class = "Daily" id="daily" name="Daily" required>
                     <option >Daily</option>
                     <option >Weekly</option>
                     <option >Monthly</option>
                     <option >Yearly</option>
-        </select>
+            </select>
+        </div>
+
+        <div class="chart-cont">
+            <div class="chart-box">
+
+            </div>
+
 
         </div>
+
+        <div class="tile-cont">
+            <div class="tiles">
+                <div class="text-cont">
+                    <p class = "label">Best Selling Product</p>
+                    <h2 class = "data">50</h2>
+                </div>
+                <div class="more-info">
+                    <a href="" id = "#">More Info</a>
+                </div>
+            </div>
+
+            <div class="tiles">
+                <div class="text-cont">
+                    <p class = "label">Total Deliveries</p>
+                    <h2 class = "data">30</h2>
+                </div>
+                <div class="more-info">
+                    <a href="" id = "#">More Info</a>
+                </div>
+            </div>
+
+            <div class="tiles">
+                <div class="text-cont">
+                    <p class = "label">Total Sold Products</p>
+                    <h2 class = "data">100</h2>
+                </div>
+                <div class="more-info">
+                    <a href="" id = "#">More Info</a>
+                </div>
+            </div>
+
+            <div class="tiles">
+                <div class="text-cont">
+                    <p class = "label">Total Sales</p>
+                    <h2 class = "data">12,000</h2>
+                </div>
+                <div class="more-info">
+                    <a href="" id = "#">More Info</a>
+                </div>
+            </div>
+        </div>
+
     </div>
 </body>
 </html>
