df4ddd6fee52eba0cae01bf33191e20940c8affd
Ahndreiy Buscado <115107347+ahndreiy02@users.noreply.github.com>
2023-09-12 00:05:13 +0800

    adminMessage

---
+++ back-end/adminMessage.php
@@ -0,0 +1,61 @@
+<?php
+    include "adminHeader.php"
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets../css/adminMessage.css">
+    <title>Document</title>
+</head>
+
+
+<body>
+<div class="mainContainer">
+        <div class="leftCont">
+            <div class="searchCont">
+                <input type="text" placeholder="Search"name="Search" id="">
+                <img src="../image/searchButton.png" alt="">
+            </div>
+
+            <div class="chatCont">
+                <div class="profileCont">
+                    <img src="../image/sample1.png" alt="">
+                </div>
+                <div class="messageCont">
+                    <p class="name">Juan Dela Cruz</p>
+                    <p class ="message">Sample Message..</p>
+                </div>
+                <div class="timeCont">
+                    <p>Mon 10:29pm</p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="rightCont">
+            <div class="inputCont">
+                <input type="text" placeholder="Type here..." name=""id="">
+                <img src="../image/clipButton.png" alt="">
+                <img src="../image/sendButton.png" alt="">
+            </div>
+
+            <div class="convoCont">
+                <div class="messCont">
+                    <p>Sample Message</p>
+                </div>
+            </div>
+        </div>
+
+
+        
+
+</div>
+</body>
+</html>
+
+   
+
